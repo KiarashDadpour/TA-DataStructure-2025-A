8b8841b23c28585286e0928ffd2506a902afaf80
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2025-03-16 13:34:15 +0330

    Update main_class.py

--- 2) Linked Lists/codes/Circularly Linked Lists/main_class.py
+++ 2) Linked Lists/codes/Circularly Linked Lists/main_class.py
@@ -1,1 +1,17 @@
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
+    def is_empty(self):
+        return self.head is None
+
+    def traversal(self):
+        if self.is_empty():
+            return "Empty List!!!"
+        current = self.head
+        while True:
+            print(current.get_info(), end="  ")
+            current = current.next
+            if current == self.head:
+                break
