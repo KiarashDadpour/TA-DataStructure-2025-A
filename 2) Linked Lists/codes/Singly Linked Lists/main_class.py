863140ab953bd0c0f1878ced0acf3b35bf0af708
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2025-03-16 12:27:56 +0330

    Update main_class.py

--- 2) Linked Lists/codes/Singly Linked Lists/main_class.py
+++ 2) Linked Lists/codes/Singly Linked Lists/main_class.py
@@ -1,45 +1,22 @@
-class Node:
-
-    def __init__(self, info, next=None):
-        self.info = info
-        self.next = next
-
-    def get_info(self):
-        return self.info
-
-    def set_info(self, value):
-        self.info = value
-
-    def get_next(self):
-        return self.next
-
-    def set_next(self, ptr):
-        self.next = ptr
-
-    def __str__(self):
-        return str(self.info)
-
-
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.last = None
-        self.size = 0
-
+        self.tail = None
+        
     def get_size(self):
-        return self.size
+        pass 
 
     def is_empty(self):
-        return self.head is None
+        return self.head is None 
 
     def traversal(self):
         if self.is_empty():
             return "Empty List!!!"
         current = self.head
         while current:
-            print(current.get_info(),end=" ==> ")
+            print(current.get_info(),end="  ")
             current = current.next
-
+        
     def search_node(self, value):
         if self.is_empty():
             return "Empty List!!!"
@@ -53,30 +30,37 @@ class LinkedList:
                 current = current.next
         return current
 
-    def insert_first(self, new: Node):
+    def insert_head(self, new: Node):
         if self.head is None:
             self.head = new
         else:
             new.next = self.head
             self.head = new
 
-    def insert_after_node(self, new: Node, old: Node):
+    def insert_tail(self, new: Node):
+        if self.head is None:
+            self.head = new
+        else:
+            self.tail.next = new
+            self.tail = new 
+
+    def insert_after_node(self, new: Node, after: Node):
         if self.is_empty():
             self.head = new
         else:
             current = self.head
             while current:
-                if current.info == old.info:
+                if current.info == after.info:
                     new.next = current.next
                     current.next = new
                     return True
-                current = current.next
+                current = current.next 
 
-    def insert_end(self, node):
-        pass
-
-
-    def delete_node(self, info):
-        pass
-
+    def delete_head(self):
+        if self.is_empty():
+            return "Empty List!!!"
+        else:
+            current = self.head 
+            self.head = self.head.get_next()
+            current.next = None 
 
